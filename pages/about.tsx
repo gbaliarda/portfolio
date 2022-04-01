928bffa5e5f0686b4abbb8d6e66efad78f80ef3e
gbaliarda <gbaliarda@itba.edu.ar>
2022-04-01 20:20:17 -0300

    Projects finished

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,9 +15,16 @@ const aboutPage: NextPage = () => {
             <hr className={styles.titleLine} />
             <div className={styles.description}>
                 <div className={styles.descriptionText}>
-                    <p className={styles.firstDescriptionText}>Hello! My name is Gonzalo Baliarda and i’m a Software Engineering student in ITBA. My interest in software started way back in high school wanting to innovate and create different solutions to any daily problem in order to automatize and increase productivity.</p>
-                    <p>I’m familiar with both Frontend and Backend development, using web frameworks such as React, Angular and Vue.</p>
-                    <p>Nowadays, i’m getting myself into Web3 and Blockchain, developing smart contracts and deploying them in testnets and localnets with Solidity and Python</p>
+                    <p className={styles.firstDescriptionText}>
+                        Hello! My name is Gonzalo Baliarda and i’m a
+                        <span className={styles.colorDescription}> Software Engineering</span> student in <span className={styles.colorDescription}>ITBA</span>.
+                        My interest in software started way back in high school
+                        wanting to <span className={styles.colorDescription}>innovate</span> and <span className={styles.colorDescription}>create</span> different solutions to any
+                        daily problem in order to automatize and increase productivity.
+                    </p>
+                    <p>I’m familiar with both Frontend and Backend development, using web frameworks such as React, NextJS, Vue and Spring.</p>
+                    <p>Nowadays, i’m getting myself into <span className={styles.colorDescription}>Web3</span> and <span className={styles.colorDescription}>Blockchain</span>, developing smart contracts and deploying them in testnets and localnets with Solidity and Python</p>
+                    <br />
                     <p>Some technologies i’ve been using lately are:</p>
                 </div>
                 <div className={styles.profileImage}>
@@ -26,7 +33,7 @@ const aboutPage: NextPage = () => {
                         <div className={styles.topRightPart}></div>
                     </div>
                     <div className={styles.imageContainer}>
-                        <img src="icono.png" alt="Profile photo" className={styles.profileImg} />
+                        <img src="profilePicture.jpg" alt="Profile photo" className={styles.profileImg} />
                     </div>
                 </div>
             </div>
@@ -36,15 +43,15 @@ const aboutPage: NextPage = () => {
                     <div className={styles.leftContent}>
                         <SkillBar skillName='Java' progress={80}></SkillBar>
                         <SkillBar skillName='C' progress={85}></SkillBar>
-                        <SkillBar skillName='HTML' progress={75}></SkillBar>
-                        <SkillBar skillName='CSS' progress={60}></SkillBar>
-                        <SkillBar skillName='JavaScript' progress={60}></SkillBar>
+                        <SkillBar skillName='HTML/CSS' progress={75}></SkillBar>
+                        <SkillBar skillName='JavaScript' progress={70}></SkillBar>
+                        <SkillBar skillName='SQL' progress={80}></SkillBar>
                     </div>
                     <div className={styles.rightContent}>
-                        <SkillBar skillName='Python' progress={40}></SkillBar>
-                        <SkillBar skillName='Solidity' progress={40}></SkillBar>
-                        <SkillBar skillName='Ruby' progress={50}></SkillBar>
-                        <SkillBar skillName='SQL' progress={80}></SkillBar>
+                        <SkillBar skillName='Python' progress={50}></SkillBar>
+                        <SkillBar skillName='Solidity' progress={50}></SkillBar>
+                        <SkillBar skillName='Ruby' progress={40}></SkillBar>
+                        <SkillBar skillName='Kotlin' progress={40}></SkillBar>
                         <SkillBar skillName='React' progress={70}></SkillBar>
                     </div>
                 </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -17,10 +17,15 @@ const projectsPage: NextPage = () => {
                 </div>
                 <hr className={styles.titleLine} />
                 <div className={styles.projects}>
-                    <ProjectCard title='Proyecto 1' github={true} linkGithub='https://www.github.com/gbaliarda' description='lorem ipsum bvla blalb labla bllb lalb aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaa a a a aa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' tech={['TEC1', 'TEC2', 'TEC3']} ></ProjectCard>
-                    <ProjectCard title='Proyecto 2' github={false} linkGithub='link' description='descripcion' tech={['abc', 'def']} ></ProjectCard>
-                    <ProjectCard title='Proyecto 3' github={false} linkGithub='link' description='descripcion' tech={['abc', 'def']} ></ProjectCard>
-                    <ProjectCard title='Proyecto 4' github={false} linkGithub='link' description='descripcion' tech={['abc', 'def']} ></ProjectCard>
+                    <ProjectCard title='CSV RealTime Data Processing' github={true} linkGithub='https://github.com/gbaliarda/csv-real-time-data-processing' description='Extract data about trees from Vancouver and Buenos Aires and process them, showing certain statistics.' tech={['C']} ></ProjectCard>
+                    <ProjectCard title='Candy Crush' github={true} linkGithub='https://github.com/gbaliarda/candy-crush-java' description='Add new funcionalities to a basic and simple Candy Crush game version.' tech={['Java']} ></ProjectCard>
+                    <ProjectCard title='SATSolver' github={true} linkGithub='https://github.com/gbaliarda/ipc-SATSolver' description='Application that resolves SAT problems using IPC mecanisms.' tech={['C']} ></ProjectCard>
+                    <ProjectCard title='64bit Shell OS' github={true} linkGithub='https://github.com/gbaliarda/64bit-shell-OS' description='Operative System based on Barebonesx64.' tech={['C', 'ASM']} ></ProjectCard>
+                    <ProjectCard title='Data extraction into DB' github={true} linkGithub='https://github.com/gbaliarda/csv-dataExtraction-toDB' description='Extract data from csv file and create a database using triggers.' tech={['PostgresSQL']} ></ProjectCard>
+                    <ProjectCard title='CTF game with Sockets' github={true} linkGithub='https://github.com/gbaliarda/sockets-ctf-game' description='Server-client application where client has to solve different challenges.' tech={['C']} ></ProjectCard>
+                    <ProjectCard title='Traning Webapp' github={true} linkGithub='https://github.com/gbaliarda/traning-webapp' description='Web application where users can create and share personal exercises and routines' tech={['Vue', 'JavaScript']} ></ProjectCard>
+                    <ProjectCard title='Traning Mobileapp' github={true} linkGithub='https://github.com/gbaliarda/traning-mobileapp' description='Mobile application where users can exercise and see their routines and personal exercises created with the webapp.' tech={['Kotlin']} ></ProjectCard>
+                    <ProjectCard title='CryptoViper' github={true} linkGithub='https://github.com/gbaliarda/cryptoviper-webapp' description='Web3 game application where users can play and earn money, deployed in the Binance Smart Chain (Testnet).' tech={['NextJS', 'JavaScript']} ></ProjectCard>
                 </div>
             </div>
         </div>
